78dbd6f698714f1d2b25fc3781436833800f1977
Matthew Croak <mattcroak718@gmail.com>
2017-06-27 15:01:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(collection)
+  new_collection = []
+  if block_given?
+    i = 0
+    while i < collection.length
+      collection.each do |i|
+        new_collection << yield(collection[i])
+        i += 1
+      end
+      new_collection
+    end
+  end
+end
